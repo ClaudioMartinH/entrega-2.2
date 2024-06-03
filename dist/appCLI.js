8bc467a9cf538a88ea09b31396ae9b55a626bf8b
ClaudioMartinH <160405164+ClaudioMartinH@users.noreply.github.com>
2024-06-03 21:24:01 +0200

    todo funciona menos cli

---
+++ dist/appCLI.js
@@ -0,0 +1,34 @@
+import { showResults } from "./index.js";
+import { JSDOM } from "jsdom";
+import readline from "readline";
+import chalk from "chalk";
+const { window } = new JSDOM(`<!DOCTYPE html><html><body>
+  <div id="width"></div>
+  <div id="height"></div>
+  <div id="contador"></div>
+</body></html>`);
+global.window = window;
+global.document = window.document;
+globalThis.contador = 0;
+console.log(chalk.blue("Bienvenido a la CLI para comprobar la funcionalidad de showResults y throttle."));
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question(chalk.green("Presiona 'Enter' para mostrar los resultados iniciales y 'r' para simular un evento de resize: "), (answer) => {
+    if (answer === "") {
+        showResults();
+    }
+    rl.on("line", (input) => {
+        if (input === "r") {
+            globalThis.contador++;
+            window.dispatchEvent(new Event("resize"));
+        }
+        console.log(chalk.yellow("Presiona 'r' para simular un evento de resize o 'q' para salir: "));
+    });
+    rl.on("close", () => {
+        console.log(chalk.red("Cerrando la CLI. ¡Adiós!"));
+        process.exit(0);
+    });
+    console.log(chalk.yellow("Presiona 'r' para simular un evento de resize o 'q' para salir: "));
+});
